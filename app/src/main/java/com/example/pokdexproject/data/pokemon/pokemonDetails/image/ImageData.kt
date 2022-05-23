36c994131fc3e1171267a5c5a1f5c2958eb370e6
WouterFaliseWasTaken <104490080+WouterFaliseWasTaken@users.noreply.github.com>
2022-05-23 17:51:55 +0200

    fixed imageView bug

--- app/src/main/java/com/example/pokdexproject/data/pokemon/pokemonDetails/image/ImageData.kt
+++ app/src/main/java/com/example/pokdexproject/data/pokemon/pokemonDetails/image/ImageData.kt
@@ -4,6 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.example.pokdexproject.network.PokemonDetailApiData
+import com.example.pokdexproject.network.Sprites
 
 @Entity(tableName = "Image")
 data class ImageData(
@@ -12,5 +13,15 @@ data class ImageData(
 )
 
 fun PokemonDetailApiData.asImageData(): List<ImageData> {
+    val list = mutableListOf<ImageData>()
+    if(!sprites.other?.officialArtwork?.frontDefault.isNullOrBlank())list.add(ImageData(sprites.other!!.officialArtwork.frontDefault,id))
+    if(!sprites.frontDefault.isNullOrBlank())list.add(ImageData(sprites.frontDefault,id))
+    if(!sprites.frontFemale.isNullOrBlank())list.add(ImageData(sprites.frontFemale,id))
+    if(!sprites.frontShiny.isNullOrBlank())list.add(ImageData(sprites.frontShiny,id))
+    if(!sprites.frontShinyFemale.isNullOrBlank())list.add(ImageData(sprites.frontShinyFemale,id))
+    if(!sprites.backDefault.isNullOrBlank())list.add(ImageData(sprites.backDefault,id))
+    if(!sprites.backFemale.isNullOrBlank())list.add(ImageData(sprites.backFemale,id))
+    if(!sprites.backShiny.isNullOrBlank())list.add(ImageData(sprites.backShiny,id))
+    if(!sprites.backShinyFemale.isNullOrBlank())list.add(ImageData(sprites.backShinyFemale,id))
     return list
 }
